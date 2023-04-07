4a1e82fcc4d3e83b801f24127cdd273e62442031
Vlad <grechishnikov.1997@mail.ru>
2023-04-07 15:27:34 +0300

    add Task 2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2 
+/*
+Console.Write("Напишите первое число для сравнения ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Напишите второе число для сравнения ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+    Console.Write("Максимамальное число " + num1);
+else
+    Console.Write("Максимальное число " + num2);
+*/
