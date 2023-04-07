12463baa79de39050bbd244cf5c789e3c5fe9f31
Vlad <grechishnikov.1997@mail.ru>
2023-04-07 16:23:30 +0300

    add Task 4

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 /*
 Console.Write("Напишите первое число для сравнения ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Напишите второе число для сравнения ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
@@ -11,3 +10,22 @@ if(num1 > num2)
 else
     Console.Write("Максимальное число " + num2);
 */
+
+//Задача 4
+
+Console.Write("Напишите первое число для сравнения ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите второе число для сравнения ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите третье число для сравнения ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if(max < num1)
+    max = num1;
+if(max < num2)
+    max = num2;
+if(max < num3)
+    max = num3;
+
+Console.Write("Максимальное число " + max);
